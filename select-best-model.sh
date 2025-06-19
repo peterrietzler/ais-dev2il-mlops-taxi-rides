ace92493f8d0067a83e00ade9a0170b24fcb6354
pri <peter.rietzler@smarter-ecommerce.com>
2025-06-19 13:29:29 +0200

    Use a python script to get the best model (bc not available on the CI server)

--- select-best-model.sh
+++ select-best-model.sh
@@ -3,10 +3,10 @@ best_model=""
 best_f1=0
 for m in "$@"; do
     f1=$(python -c "import json; print(json.load(open('$m'))['True']['f1-score'])")
-    if (( $(echo "$f1 > $best_f1" | bc -l) )); then
+    if python -c "exit(0) if float('$f1') > float('$best_f1') else exit(1)"; then
         best_f1=$f1
         best_model=$m
     fi
 done
-echo "${best_model} i the best model with f1-score ${best_f1}"
+echo "${best_model} is the best model with f1-score ${best_f1}"
 cp "${best_model%.metadata.json}" "model"
